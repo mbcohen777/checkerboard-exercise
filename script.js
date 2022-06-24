0456d844942f685ef25e03c8abf2a70a20b88f81
mbcohen777 <107497234+mbcohen777@users.noreply.github.com>
2022-06-24 10:29:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your JS goes here
+var container = document.createElement('div')
+document.body.append(container)
+
+function getColor() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);  
+    var color = "rgb("+r+","+g+","+b+")"
+    return color;
+}
+
+for(var i=0;i<81;i++) {
+    var div = document.createElement('div');
+    div.style.width = '11.1%';
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    div.style.background = getColor();
+    // if(i %2 === 0) {
+    //     div.style.background = 'red';
+    // }
+    // else {
+    //     div.style.background = 'black';
+    // }
+    // div.style.border = '1px solid black';
+    container.append(div);
+}
